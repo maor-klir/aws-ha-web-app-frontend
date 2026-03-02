dedb666d0145914e84b895b40238667ca8cd260c
Maor Klir <maor.klir@gmail.com>
2026-03-03 00:33:12 +0100

    feat: transition to and force using EC2 IMDSv2
    Signed-off-by: Maor Klir <maor.klir@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ module "networking" {
 
 module "ec2" {
   source  = "app.terraform.io/maor/terraform-aws-ec2/aws"
-  version = "0.2.0"
+  version = "0.3.0"
 
   aws_region          = var.aws_region
   ec2_instance_type   = var.ec2_instance_type
